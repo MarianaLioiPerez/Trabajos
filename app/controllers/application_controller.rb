60ba52a213103c0377aaf8091d81c6c6931f4ff3
MarianaLioiPerez <lioiperezm@gmail.com>
2023-08-07 21:11:31 -0400

    problemas de rutas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
     private
 
     def authorize_admin
-      unless current_tuser&.admin?
-        flash[:alert] = "You are not authorized to perform this action."
+      unless current_tuser  && current_tuser.admin?
+        flash[:alert] = "Acceso denegado."
         redirect_to root_path
       end
     end
